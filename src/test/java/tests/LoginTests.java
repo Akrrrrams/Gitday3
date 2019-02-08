4eeeb7108ef2a44b34019a0382301f0afe37c5be
Akram <akins7559@gmail.com>
2019-02-08 18:40:39 -0500

    update

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -15,45 +15,50 @@ public class LoginTests {
     WebDriver driver;
 
     @BeforeMethod
-    public void setup(){
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
     }
+
     @Test
-    public void negetiveloginTest1(){
+    public void loginTest1() {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester23");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-<<<<<<< HEAD
-        Assert.assertEquals(driver.getTitle(),"Web Orders")
-=======
+        Assert.assertEquals(driver.getTitle(), "Web Orders");
+    }
+
+
+
+    @Test
+    public void logOutTest() {
+        driver .get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        driver.findElement(By.id("ctl00_logout")).click();
+        String str = "ramile";
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
+    }
+
+
+
+    @Test
+    public void negativeloginTest() {
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester2");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test2" + Keys.ENTER);
         String errorMsg = driver.findElement(By.id("ctl00_MainContent_status")).getText();
 
         Assert.assertEquals(errorMsg, "Invalid Login or Password.");
->>>>>>> feature
     }
 
 
     @AfterMethod
-    public void closeDriver(){
+    public  void cleanUp(){
+
         driver.close();
     }
-
-    @Test
-    public void logoutTest(){
-        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
-        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
-        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
-        driver.findElement(By.id("ctl00_logout")).click();
-       //String  add some master shitg
-        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
-
-
-    }
-
-
 
 
     }
